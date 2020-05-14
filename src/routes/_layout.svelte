656b7ad7d772b7cf35cb4df86cafa61684a2c1ea
Diego Kfuri <kkfuri@gmail.com>
2020-05-14 13:44:43 -0300

    Remove cypress e adiciona links no layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -7,10 +7,63 @@
     margin: 0 auto;
     box-sizing: border-box;
   }
+
+  footer {
+    text-align: center;
+  }
+
+  h6 {
+    font-size: 16px;
+    font-family: monospace;
+  }
+  a {
+    color: #333;
+    text-decoration: none;
+    transition: 0.2s;
+  }
+
+  a:hover {
+    font-size: 110%;
+  }
+
+  .github {
+    position: absolute;
+    right: 2em;
+    z-index: 10;
+  }
 </style>
 
 <div style="min-height: 100vh">
   <main>
+    <div class="github">
+      <a
+        target="_blank"
+        rel="noopener"
+        href="https://github.com/kkfuri/svelte-covid"
+        aria-label="Ir para o github do projeto">
+        projeto no github
+      </a>
+    </div>
     <slot />
   </main>
+  <footer>
+    <h6>
+      Made by
+      <a
+        target="_blank"
+        rel="noopener"
+        href="https://github.com/kkfuri"
+        aria-label="Ir para o github do desenvolvedor">
+        Diego
+      </a>
+      with
+      <a
+        target="_blank"
+        rel="noopener"
+        href="https://sapper.svelte.dev/"
+        aria-label="Ir para página do Sapper">
+        Sapper
+      </a>
+    </h6>
+  </footer>
 </div>
