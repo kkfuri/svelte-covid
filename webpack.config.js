8289281ed803b27f5de36325866e93fe2ba3fd05
Diego Kfuri <kkfuri@gmail.com>
2020-09-28 17:43:59 -0300

    feat: run yarn upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require("webpack");
+const WebpackModules = require("webpack-modules");
 const path = require("path");
 const config = require("sapper/config/webpack.js");
 const pkg = require("./package.json");
@@ -57,13 +58,15 @@ module.exports = {
             options: {
               css: false,
               generate: "ssr",
+              hydratable: true,
               dev,
             },
           },
         },
       ],
     },
-    mode: process.env.NODE_ENV,
+    mode,
+    plugins: [new WebpackModules()],
     performance: {
       hints: false, // it doesn't matter if server.js is large
     },
@@ -72,6 +75,6 @@ module.exports = {
   serviceworker: {
     entry: config.serviceworker.entry(),
     output: config.serviceworker.output(),
-    mode: process.env.NODE_ENV,
+    mode,
   },
 };
